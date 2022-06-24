c0902c8fcef7d24a7a7c12ddc7aca827ead8b01e
Diogo Torres Correia <diogo.t.correia@tecnico.ulisboa.pt>
2022-06-24 18:04:11 +0100

    Add `/about` endpoint

---
+++ src/main/java/org/fenixedu/api/ui/BaseController.java
@@ -0,0 +1,64 @@
+package org.fenixedu.api.ui;
+
+import com.google.gson.JsonObject;
+import org.fenixedu.academic.domain.ExecutionSemester;
+import org.fenixedu.academic.domain.ExecutionYear;
+import org.fenixedu.academic.domain.organizationalStructure.Unit;
+import org.fenixedu.bennu.core.json.JsonUtils;
+
+import java.util.Locale;
+
+public class BaseController extends org.fenixedu.bennu.spring.BaseController {
+
+    static JsonObject toUnitJson(Unit unit) {
+        if (unit == null) {
+            return null;
+        }
+        return JsonUtils.toJson(institution -> {
+            institution.addProperty("name", unit.getName());
+            if (unit.hasDefaultWebAddress()) {
+                institution.addProperty("url", unit.getDefaultWebAddressUrl());
+            }
+            if (unit.hasDefaultEmailAddress()) {
+                institution.addProperty("email", unit.getDefaultEmailAddressValue());
+            }
+            if (unit.hasDefaultMobilePhone()) {
+                institution.addProperty("phone", unit.getDefaultMobilePhoneNumber());
+            }
+        });
+    }
+
+    static JsonObject toExecutionSemesterJson(ExecutionSemester executionSemester) {
+        if (executionSemester == null) {
+            return null;
+        }
+        return JsonUtils.toJson(semester -> {
+            semester.addProperty("displayName", executionSemester.getQualifiedName());
+            semester.addProperty("semester", executionSemester.getSemester());
+            semester.addProperty("beginDate", executionSemester.getBeginLocalDate().toString());
+            semester.addProperty("endDate", executionSemester.getEndLocalDate().toString());
+            JsonUtils.addIf(semester, "year", toExecutionYearJson(executionSemester.getExecutionYear()));
+        });
+    }
+
+    static JsonObject toExecutionYearJson(ExecutionYear executionYear) {
+        if (executionYear == null) {
+            return null;
+        }
+        return JsonUtils.toJson(year -> {
+            year.addProperty("displayName", executionYear.getQualifiedName());
+            year.addProperty("beginYear", executionYear.getBeginCivilYear());
+            year.addProperty("endYear", executionYear.getEndCivilYear());
+            year.addProperty("beginDate", executionYear.getBeginLocalDate().toString());
+            year.addProperty("endDate", executionYear.getEndLocalDate().toString());
+        });
+    }
+
+    static JsonObject toLocaleJson(Locale locale) {
+        return JsonUtils.toJson(localeJson -> {
+            localeJson.addProperty("locale", locale.toLanguageTag());
+            localeJson.addProperty("name", locale.getDisplayName());
+        });
+    }
+
+}
