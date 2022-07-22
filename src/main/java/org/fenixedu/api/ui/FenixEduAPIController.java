b465a957458ec1d8d7c45e8562b9bccbe32f3479
Diogo Marques Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-07-22 23:02:11 +0100

    Add academic terms endpoint

--- src/main/java/org/fenixedu/api/ui/FenixEduAPIController.java
+++ src/main/java/org/fenixedu/api/ui/FenixEduAPIController.java
@@ -2,17 +2,18 @@ package org.fenixedu.api.ui;
 
 import com.google.gson.JsonObject;
 import org.fenixedu.academic.domain.ExecutionSemester;
+import org.fenixedu.academic.domain.ExecutionYear;
 import org.fenixedu.academic.domain.organizationalStructure.Unit;
-import org.fenixedu.api.util.APIScope;
+import org.fenixedu.api.util.APIError;
 import org.fenixedu.bennu.core.domain.Bennu;
 import org.fenixedu.bennu.core.json.JsonUtils;
 import org.fenixedu.bennu.core.util.CoreConfiguration;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/fenixedu-api/v2")
@@ -31,7 +32,7 @@ public class FenixEduAPIController extends BaseController {
 
         return ok(data -> {
             JsonUtils.addIf(data, "institution", toUnitJson(institution));
-            JsonUtils.addIf(data, "activeSemester", toExecutionSemesterJson(currentSemester));
+            JsonUtils.addIf(data, "activeSemester", toExecutionSemesterJson(currentSemester, true));
             data.add("languages", JsonUtils.toJsonArray(languages -> CoreConfiguration.supportedLocales().stream()
                     .map(this::toLocaleJson)
                     .forEach(languages::add))
@@ -40,6 +41,38 @@ public class FenixEduAPIController extends BaseController {
 
             // FIXME add RSS feed urls (?)
         });
+    }
+
+    // gets all terms
+    @RequestMapping(value = "/academicterms", method = RequestMethod.GET)
+    public ResponseEntity<?> academicTerms(@RequestParam(required = false) Optional<String> from) {
+        final Bennu bennu = Bennu.getInstance();
+
+        if (from.isPresent()) {
+            final ExecutionYear firstExecutionYear = Optional.ofNullable(ExecutionYear.readExecutionYearByName(from.get()))
+                    .orElseThrow(() -> new APIError(HttpStatus.BAD_REQUEST, "error.academicterm.year.incorrect", from.get()));
+
+            return ok(bennu.getExecutionYearsSet().stream()
+                    .filter(executionYear -> executionYear.isAfterOrEquals(firstExecutionYear))
+                    .sorted(ExecutionYear.REVERSE_COMPARATOR_BY_YEAR)
+                    .map(executionYear -> toExecutionYearJson(executionYear, true))
+                    .collect(jsonArrayCollector)
+            );
+        }
+        return ok(bennu.getExecutionYearsSet().stream()
+                .sorted(ExecutionYear.REVERSE_COMPARATOR_BY_YEAR)
+                .map(executionYear -> toExecutionYearJson(executionYear, true))
+                .collect(jsonArrayCollector)
+        );
+    }
+
+    // gets a specific term
+    @RequestMapping(value = "/academicterms/{beginYear}/{endYear}", method = RequestMethod.GET)
+    public ResponseEntity<?> getAcademicTerm(@PathVariable String beginYear, @PathVariable String endYear) {
+        String year = beginYear + "/" + endYear;
+        final ExecutionYear executionYear = Optional.ofNullable(ExecutionYear.readExecutionYearByName(year))
+                .orElseThrow(() -> new APIError(HttpStatus.BAD_REQUEST, "error.academicterm.year.incorrect", year));
+        return ok(toExecutionYearJson(executionYear, true));
     }
 
 }
