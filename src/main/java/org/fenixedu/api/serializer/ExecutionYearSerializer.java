a57ec84429e72e6746dfb696e09cf0f52ba107fb
Diogo Correia <diogo.t.correia@tecnico.ulisboa.pt>
2023-02-06 18:37:00 +0000

    Send array of semesters instead of first and second semesters
    This avoids future problems and better represents the underlying domain.
    It also fixes a bug where the /academicterms endpoint would crash if
    there was an execution year with less that 2 semesters.

--- src/main/java/org/fenixedu/api/serializer/ExecutionYearSerializer.java
+++ src/main/java/org/fenixedu/api/serializer/ExecutionYearSerializer.java
@@ -1,9 +1,9 @@
 package org.fenixedu.api.serializer;
 
 import com.google.gson.JsonObject;
-import org.fenixedu.academic.domain.ExecutionSemester;
 import org.fenixedu.academic.domain.ExecutionYear;
 import org.fenixedu.bennu.core.json.JsonUtils;
+import org.fenixedu.commons.stream.StreamUtils;
 import org.jetbrains.annotations.NotNull;
 
 public class ExecutionYearSerializer extends DomainObjectSerializer {
@@ -25,16 +25,11 @@ public class ExecutionYearSerializer extends DomainObjectSerializer {
     public @NotNull JsonObject serializeExtended(@NotNull ExecutionYear executionYear) {
         JsonObject data = serialize(executionYear);
         data.add(
-                "firstSemester",
-                this.getAPISerializer()
-                        .getExecutionSemesterSerializer()
-                        .serialize(executionYear.getFirstExecutionPeriod())
-        );
-        data.add(
-                "secondSemester",
-                this.getAPISerializer()
-                        .getExecutionSemesterSerializer()
-                        .serialize(executionYear.getLastExecutionPeriod())
+                "semesters",
+                executionYear.getExecutionPeriodsSet()
+                        .stream()
+                        .map(this.getAPISerializer().getExecutionSemesterSerializer()::serialize)
+                        .collect(StreamUtils.toJsonArray())
         );
         return data;
     }
